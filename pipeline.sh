c8ce470d6823bfc8c813ac51601d5b6b96646f0f
berra <berra@DESKTOP-JSBN2TS.localdomain>
2026-09-25 23:05:50 +0300

    Fix pipeline reference and result outputs

--- pipeline.sh
+++ pipeline.sh
@@ -4,7 +4,8 @@
 set -e
 
 echo "=== 1. Adım: Çalışma Dizininin Oluşturulması ==="
-mkdir -p data && cd data
+mkdir -p data results
+cd data
 
 echo "=== 2. Adım: Ham Verinin İndirilmesi (SRR17855325) ==="
 fasterq-dump SRR17855325 --split-files
@@ -18,9 +19,8 @@ fastp -i SRR17855325_1.fastq -I SRR17855325_2.fastq \
       --html fastp_rapor.html --json fastp_rapor.json
 
 echo "=== 5. Adım: Referans Genomun İndirilmesi ve Hazırlanması ==="
-wget -nc https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-gunzip -k -f GCF_000005845.2_ASM584v2_genomic.fna.gz
-mv GCF_000005845.2_ASM584v2_genomic.fna referans.fasta
+wget -O NC_045512.2.fasta "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=NC_045512.2&rettype=fasta&retmode=text"
+mv NC_045512.2.fasta referans.fasta
 
 echo "=== 6. Adım: Referans Genom İndeksleme ==="
 bwa index referans.fasta
@@ -32,12 +32,15 @@ bwa mem referans.fasta temiz_1.fastq temiz_2.fastq > hizalama.sam
 echo "=== 8. Adım: SAM -> BAM Dönüştürme, Sıralama ve İndeksleme ==="
 samtools view -bS hizalama.sam | samtools sort -o hizalama_sirali.bam
 samtools index hizalama_sirali.bam
+samtools flagstat hizalama_sirali.bam > ../results/mapping_stats.txt
 
 echo "=== 9. Adım: Varyant Çağırma (bcftools) ==="
 bcftools mpileup -f referans.fasta hizalama_sirali.bam | bcftools call -mv -Ob -o varyantlar.bcf
+bcftools view varyantlar.bcf -Ov -o ../results/sars_cov2_variants.vcf
+bcftools stats varyantlar.bcf > ../results/variant_stats.txt
 
-echo "=== 10. Adım: CSV Formatında Tablolaştırma ==="
-bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%QUAL\n' varyantlar.bcf | tr '\t' ',' > varyantlar.csv
+echo "=== 10. Adım: TSV Formatında Varyant Tablosu Oluşturma ==="
+bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%INFO/DP\n' varyantlar.bcf > ../results/variants.tsv
 
 echo "🎉 Tebrikler! SRR17855325 verisine ait tüm pipeline uçtan uca tamamlandı."
-echo "Çıktı dosyaları ve 'varyantlar.csv' verisi 'data' klasöründe hazır!"
+echo "🎉 Pipeline tamamlandı! Sonuçlar 'results' klasöründe hazır."
